1c4e4c4291489ee5fe01c6df0161bc7e451296cc
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-07-14 12:15:34 +0530

    first commit

---
+++ src/first1.java
@@ -0,0 +1,16 @@
+public class first1 {
+    public static void main(String[] args) {
+        int count = 10;
+        System.out.println("count value "+count);
+        count =20;
+        System.out.println("count value "+count);
+        double interest = 5.2;
+        String name = "pavani";
+        float interestf = 5.2f;
+        
+
+        System.out.println("interest rate is "+interestf); 
+
+    }
+    
+}
